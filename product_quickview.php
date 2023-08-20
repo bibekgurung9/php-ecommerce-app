710adb27a5223308c0d7408c2bedd8536e15cdb8
Bibek Gurung <100699318+bibekgurung9@users.noreply.github.com>
2023-08-20 23:25:57 +0545

    product description iteration 1

--- product_quickview.php
+++ product_quickview.php
@@ -10,6 +10,23 @@ if(!isset($user_id)){
    header('location:login.php');
 }
 
+if(isset($_POST['add_to_cart'])){
+
+   $product_name = $_POST['product_name'];
+   $product_price = $_POST['product_price'];
+   $product_image = $_POST['product_image'];
+   $product_quantity = $_POST['product_quantity'];
+
+   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
+
+   if(mysqli_num_rows($check_cart_numbers) > 0){
+      $message[] = 'already added to cart!';
+   }else{
+      mysqli_query($conn, "INSERT INTO `cart`(user_id, name, price, quantity, image) VALUES('$user_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+      $message[] = 'product added to cart!';
+   }
+
+}
 ?>
 
 <!DOCTYPE html>
@@ -40,15 +57,21 @@ if(!isset($user_id)){
       ?>
      <form action="" method="post" class="box">
       <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="Product Image Could Not Load Correctly">
+      
+      <div class="product-details">
       <div class="name"><?php echo $fetch_products['name']; ?></div>
-      <div class="description"><?php echo $fetch_products['description']?></div>
-      <div class="price">Rs.<?php echo $fetch_products['price']; ?>/-</div>
+      <div class="description"><b>Product Description:</b><br><?php echo $fetch_products['description']?></div>
+      </div>
+            <div class="price">Rs.<?php echo $fetch_products['price']; ?>/-</div>
+      
+      <div class="qty-cart">
       <input type="number" min="1" name="product_quantity" value="1" class="qty">
       <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
       <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
       <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
       <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-     </form>
+      </div>
+           </form>
       <?php
          }
       }else{
